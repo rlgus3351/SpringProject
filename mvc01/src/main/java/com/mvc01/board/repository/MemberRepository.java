beacdea4decd04e7173d5e69238fd2b094c67f30
권기현 <rlgus3351@naver.com>
2023-08-13 23:17:31 +0900

    23/08/13 DATABASE ERROR

--- mvc01/src/main/java/com/mvc01/board/repository/MemberRepository.java
+++ mvc01/src/main/java/com/mvc01/board/repository/MemberRepository.java
@@ -1,14 +1,19 @@
 package com.mvc01.board.repository;
 
 import com.mvc01.board.dto.MemberDTO;
+import lombok.RequiredArgsConstructor;
 import org.apache.ibatis.reflection.SystemMetaObject;
+import org.apache.ibatis.session.SqlSession;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@RequiredArgsConstructor
 public class MemberRepository {
+    private final SqlSessionTemplate sql;
 
     public int save(MemberDTO memberDTO) {
         System.out.println("member DTO = " + memberDTO);
-        return 0;
+        return sql.insert("Member.save",memberDTO);
     }
 }
